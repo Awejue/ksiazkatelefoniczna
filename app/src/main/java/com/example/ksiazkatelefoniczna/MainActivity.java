bbea8f7ac7dcd56470e13294e70789ccf4a2eee7
Awejue <awejueee@gmail.com>
2023-05-14 22:34:16 +0200

    men

--- app/src/main/java/com/example/ksiazkatelefoniczna/MainActivity.java
+++ app/src/main/java/com/example/ksiazkatelefoniczna/MainActivity.java
@@ -35,13 +35,10 @@ public class MainActivity extends AppCompatActivity {
         // Tworzenie tabeli, jeśli nie istnieje
         mDb.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, last_name TEXT, country TEXT, phone TEXT, avatar BLOB)");
 
+        TextView search = findViewById(R.id.search);
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(view -> showDataInTable("%"+search.getText().toString()+"%"));
 
-        /*ContentValues values = new ContentValues();
-        values.put("name", "Aflonoanosnda");
-        values.put("last_name", "Spien");
-        values.put("country", "Golkowice");
-        values.put("phone", "123456798");
-        mDb.insert(TABLE_NAME,null, values);*/
 
         Button AddRecord = findViewById(R.id.dodaj);
         AddRecord.setOnClickListener(view -> {
@@ -61,6 +58,60 @@ public class MainActivity extends AppCompatActivity {
 
         // Pobieranie danych z bazy danych
         Cursor cursor = mDb.query(TABLE_NAME, null, null, null, null, null, null);
+
+        // Dodawanie nagłówków tabeli
+        TableRow headerRow = new TableRow(this);
+        headerRow.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+        String[] headerText = {"ID", "Imie", "Nazwisko", "Miejscowosc", "Numer"};
+        for (String s : headerText) {
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            tv.setTextSize(18);
+            tv.setPadding(5, 5, 5, 5);
+            tv.setText(s);
+            headerRow.addView(tv);
+        }
+        table.addView(headerRow);
+
+        while (cursor.moveToNext()) {
+            dane = new String[] {String.valueOf(cursor.getInt(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4)};
+            TableRow row= new TableRow(this);
+            row.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT));
+            for (String s : dane) {
+                TextView tv = new TextView(this);
+                tv.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+                tv.setTextSize(18);
+                tv.setPadding(5, 5, 5, 5);
+                tv.setText(s);
+                tv.setId(Integer.parseInt(dane[0]));
+                tv.setOnLongClickListener(view -> {
+                    id = String.valueOf(view.getId());
+                    Intent intentMain = new Intent(MainActivity.this,
+                            Modify.class);
+                    MainActivity.this.startActivity(intentMain);
+                    return true;
+                });
+                row.addView(tv);
+            }
+            table.addView(row);
+        }
+        cursor.close();
+    }
+
+    protected void showDataInTable(String args) {
+        TableLayout table = findViewById(R.id.table);
+        table.removeAllViewsInLayout();
+
+        // Pobieranie danych z bazy danych
+        Cursor cursor = mDb.query(TABLE_NAME, null, "id == ? OR name LIKE ? OR last_name LIKE ? OR country LIKE ? OR phone LIKE ?", new String[]{args,args,args,args,args}, null, null, null);
 
         // Dodawanie nagłówków tabeli
         TableRow headerRow = new TableRow(this);
